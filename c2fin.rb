91877670fad7b53c5668b7fcaf573443274384d8
jleo3 <joseph.leo3@gmail.com>
2017-08-20 11:59:08 -0400

    more c2f

---
+++ c2fin.rb
@@ -0,0 +1,7 @@
+puts "Reading Celsius temparature value from data file..."
+num = File.read("temp.dat")
+celsius = num.to_i
+fahrenheit = (celsius * 9 / 5) + 32
+puts "The number is " + num
+print "Result: "
+puts fahrenheit
---
+++ c2fout.rb
@@ -0,0 +1,7 @@
+puts "Hello. Please enter a Celsius value: "
+celsius = gets.to_i
+fahrenheit = (celsius * 9 / 5) + 32
+puts "Saving result to output file 'temp.out'"
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close
