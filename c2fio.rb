dbc2c64a1db67fa8a30dad7c78b3093d3923bf43
jleo3 <joseph.leo3@gmail.com>
2017-08-20 13:33:11 -0400

    more more c2f

--- c2fio.rb
+++ c2fio.rb
@@ -2,6 +2,7 @@ puts "Reading Celsius temperature value from data file..."
 num = File.read("temp.dat")
 celsius = num.to_i
 fahrenheit = (celsius * 9 / 5) + 32
-puts "The number is " + num
-print "Result: "
-puts fahrenheit
+puts "Saving result to output file 'temp.out'"
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close
