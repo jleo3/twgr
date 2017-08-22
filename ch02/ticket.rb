9b97082259d7f84fffe7f520b4455aba11e2eadf
jleo3 <joseph.leo3@gmail.com>
2017-08-22 12:28:44 -0400

    ticket object

---
+++ ch02/ticket.rb
@@ -0,0 +1,25 @@
+ticket = Object.new
+
+def ticket.date
+ "01/02/03"
+end
+def ticket.venue
+ "Town Hall"
+end
+def ticket.event
+ "Author's reading"
+end
+def ticket.performer
+ "Mark Twain"
+end
+def ticket.seat
+ "Second Balcony, row J, seat 12"
+end
+def ticket.price
+ 5.50
+end
+
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}.\n" +
+ "The performer is #{ticket.performer}.\n" +
+ "The seat is #{ticket.seat}, " +
+ "and it costs $#{"%.2f." % ticket.price}"
