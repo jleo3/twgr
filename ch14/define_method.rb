a4b4680e6154d9156d7f67c56c81d183f89871ae
jleo3 <joseph.leo3@gmail.com>
2018-07-26 11:00:49 -0400

    add define_method example

---
+++ ch14/define_method.rb
@@ -0,0 +1,32 @@
+class Name
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def to_s
+    instance_variables.each do |ivar|
+      print "#{instance_variable_get(ivar)} "
+    end
+    puts
+  end
+
+  def method_missing(m, args, &block)
+    if m.to_s.include?("_name=")
+      self.class.send(:define_method, m) do |args|
+        instance_variable_set(:"@#{m.to_s.chop}", args)
+      end
+      send(m, args)
+    else
+      raise "No method for #{m}!"
+    end
+  end
+end
+
+n = Name.new("Joe", "Leo")
+puts n.instance_variables
+n.to_s
+n.middle_name = "Phillip"
+puts n.instance_variables
+n.to_s
+n.initials = "JPL"
